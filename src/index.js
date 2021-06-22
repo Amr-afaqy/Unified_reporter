2efe18024baca2145d1f98509d185d40c014afc5
Amr Aly <amrahmed2089@gmail.com>
2021-06-22 04:56:14 +0200

    Push test results fix

--- src/index.js
+++ src/index.js
@@ -1,107 +1,88 @@
-const Distributer = require("./distributer")
+const Distributer = require("./distributer");
 
 class TestRun {
-    constructor() {
-        this.dataObject = {
-            reportStart: "",
-            reportAgent: "",
-            testsCount: "",
-            testFixtures: [],
-            rendTime: "",
-            rpassed: "",
-            rfailed: "",
-            rwarnings: "",
-            rresults: "",
-        };
-    }
+  constructor() {
+    this.dataObject = {
+      reportStart: "",
+      reportAgent: "",
+      testsCount: "",
+      testFixtures: [],
+      rendTime: "",
+      rpassed: "",
+      rfailed: "",
+      rwarnings: "",
+      rresults: "",
+    };
+  }
 
-    updateDataObject(params, value) {
-        this.dataObject[params] = value;
-    }
+  updateDataObject(params, value) {
+    this.dataObject[params] = value;
+  }
 
-    updateFixtures(fName, fPath, fMeta, fID) {
-        this.dataObject["testFixtures"].push({
-            fID: fID,
-            name: fName,
-            path: fPath,
-            fMeta: fMeta,
-            fTests: [],
-        });
-    }
+  updateFixtures(fName, fPath, fMeta, fID) {
+    this.dataObject["testFixtures"].push({
+      fID: fID,
+      name: fName,
+      path: fPath,
+      fMeta: fMeta,
+      fTests: [],
+    });
+  }
 
-    updateFixtureTests(tName, tMeta, tID, info) {
-        this.dataObject["testFixtures"][
-            this.dataObject["testFixtures"].length - 1
-        ]["fTests"].push({
-            tID: tID,
-            tName: tName,
-            tMeta: tMeta,
-            runInfo: info,
-        });
-    }
+  updateFixtureTests(tName, tMeta, tID, info) {
+    this.dataObject["testFixtures"][this.dataObject["testFixtures"].length - 1]["fTests"].push({
+      tID: tID,
+      tName: tName,
+      tMeta: tMeta,
+      runInfo: info,
+    });
+  }
 
-    updateTestRunFooter(endTime, passed, warnings, result) {
-        this.dataObject["rendTime"] = endTime;
-        this.dataObject["rpassed"] = passed;
-        this.dataObject["rfailed"] = passed;
-        this.dataObject["rwarnings"] = warnings;
-        this.dataObject["rresults"] = result;
-    }
+  updateTestRunFooter(endTime, passed, warnings, result) {
+    this.dataObject["rendTime"] = endTime;
+    this.dataObject["rpassed"] = passed;
+    this.dataObject["rfailed"] = passed;
+    this.dataObject["rwarnings"] = warnings;
+    this.dataObject["rresults"] = result;
+  }
 
-    async printGeneratedObject() {
-        try{
-            console.log("Distributing......")
-            const configFile = require('./../config.json')
-            const reportDistributer = new Distributer();
-            await reportDistributer.setConfigFile(configFile);
-            await reportDistributer.setTestObject(this.dataObject);
-            await reportDistributer.startDistributing()
-            console.log("Done......")
-        }catch(error){
-            console.error()
-            console.log(error)
-        }
+  async printGeneratedObject() {
+    try {
+      console.log("Distributing......");
+      const configFile = require("./../config.json");
+      const reportDistributer = new Distributer();
+      await reportDistributer.setConfigFile(configFile);
+      await reportDistributer.setTestObject(this.dataObject);
+      await reportDistributer.startDistributing();
+      console.log("Done......");
+    } catch (error) {
+      console.error();
+      console.log(error);
     }
+  }
 }
 
 module.exports = function () {
-    return {
-        noColors: true,
-        reporterHandler: new TestRun(),
-        reportTaskStart(startTime, userAgents, testCount) {
-            this.reporterHandler.updateDataObject("reportStart", startTime);
-            this.reporterHandler.updateDataObject("reportAgent", userAgents);
-            this.reporterHandler.updateDataObject("testsCount", testCount);
-        },
+  return {
+    noColors: true,
+    reporterHandler: new TestRun(),
+    reportTaskStart(startTime, userAgents, testCount) {
+      this.reporterHandler.updateDataObject("reportStart", startTime);
+      this.reporterHandler.updateDataObject("reportAgent", userAgents);
+      this.reporterHandler.updateDataObject("testsCount", testCount);
+    },
 
-        reportFixtureStart(name, path, meta) {
-            this.reporterHandler.updateFixtures(
-                name,
-                path,
-                meta,
-                Math.random()
-            );
-        },
+    reportFixtureStart(name, path, meta) {
+      this.reporterHandler.updateFixtures(name, path, meta, Math.random());
+    },
 
-        reportTestDone(name, testRunInfo, meta) {
-            this.reporterHandler.updateFixtureTests(
-                name,
-                meta,
-                Math.random(),
-                testRunInfo
-            );
-        },
+    reportTestDone(name, testRunInfo, meta) {
+      this.reporterHandler.updateFixtureTests(name, meta, Math.random(), testRunInfo);
+    },
 
-        async reportTaskDone(endTime, passed, warnings, result) {
-            this.reporterHandler.updateTestRunFooter(
-                endTime,
-                passed,
-                warnings,
-                result
-            );
-           await this.reporterHandler.printGeneratedObject();
-        },
-    };
+    async reportTaskDone(endTime, passed, warnings, result) {
+      this.reporterHandler.updateTestRunFooter(endTime, passed, warnings, result);
+      await this.reporterHandler.printGeneratedObject();
+    },
+  };
 };
-
-
