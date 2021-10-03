bde47b8277a43b43c9081c1127ccaff1d2365a1e
Amrrx <amrahmed2089@gmail.com>
2021-10-03 17:20:19 +0200

    Start unit testing

---
+++ src/distributer.mjs
@@ -0,0 +1,49 @@
+"use strict";
+const JiraCore = require("./core/JiraCore.js")
+const TestrailCore = require("./core/TestrailCore.js")
+const TestRailInstance = require("./channels/TestRailInstance.js")
+const jiraInstance = require("./channels/jiraInstance.js")
+const logger = require("./logger");
+const globalConfigs = require("../config.json");
+
+module.exports = class Distributer {
+   #TEST_OBJECT;
+   constructor() { }
+
+   async setTestObject(testObject) {
+      this.#TEST_OBJECT = testObject;
+   }
+
+   async startDistributing() {
+      await this.#distributeToRail(this.#TEST_OBJECT);
+      await this.#distributeToJira(this.#TEST_OBJECT);
+   }
+
+   async #distributeToRail(railTestObject) {
+      try {
+         const railCore = new TestrailCore(globalConfigs)
+         const railObject = new TestRailInstance(railCore, globalConfigs);
+         const authToken = await railObject.createTokenDetails();
+         const runData = await railObject.prepareRunData(authToken, railTestObject);
+         await railObject.pushNewRun(authToken, runData);
+         await railObject.pushTestResults(authToken, runData);
+      } catch (error) {
+         logger(error, true);
+         console.error(error)
+      }
+   }
+
+   async #distributeToJira(testObject) {
+      try {
+         const jiraCore = new JiraCore(globalConfigs)
+         let jira = new jiraInstance(jiraCore, globalConfigs);
+         let sessionCookies = await jira.initateAuthenticationToken();
+         let defects = await jira.extractDefectsFromObject(testObject);
+         let issuesList = await jira.createIssueObjectList(defects);
+         await jira.pushEachDefect(await sessionCookies, issuesList);
+      } catch (error) {
+         logger(error, true);
+         console.error(error)
+      }
+   }
+};
